f1760c3833c1d2dcb9df023d60ab1d91ef0ffbf2
rayhanp1402 <rayhan.hanif14maulana@gmail.com>
2023-05-04 04:24:32 +0700

    feat: Levensthein Distance and Similarity Test

--- src/features/stringMatching.go
+++ src/features/stringMatching.go
@@ -112,6 +112,50 @@ func generateMatchedIdx(pattern string, text string, idx int) []int {
 	return matchIndexes
 }
 
+func min(nums ...int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	res := nums[0]
+	for _, num := range nums {
+		if num < res {
+			res = num
+		}
+	}
+	return res
+}
+
+func levenstheinDistance(pattern string, text string) int {
+	m := len(pattern)
+	n := len(text)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+		dp[i][0] = i
+	}
+	for j := 1; j <= n; j++ {
+		dp[0][j] = j
+	}
+	for j := 1; j <= n; j++ {
+		for i := 1; i <= m; i++ {
+			if pattern[i-1] == text[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = min(dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1]+1)
+			}
+		}
+	}
+	return dp[m][n]
+}
+
+func calculateSimilarity(pattern string, text string) float64 {
+	if len(pattern) >= len(text) {
+		return (1 - float64(levenstheinDistance(pattern, text))/float64(len(pattern))) * 100
+	} else {
+		return (1 - float64(levenstheinDistance(pattern, text))/float64(len(text))) * 100
+	}
+}
+
 // Driver
 func stringMatchingDriver() {
 	var hello string = "hello, World!"
@@ -140,5 +184,35 @@ func stringMatchingDriver() {
 	fmt.Println(boyerMoore("aaab", "aaaaaaab"))
 	fmt.Println(boyerMoore("BAB", "ABABABAC"))
 	fmt.Println(boyerMoore("TEST", "THIS IS A TEST"))
+	fmt.Println()
+
+	fmt.Println("Levensthein Distance-test:")
+	s1 := "kitten"
+	t1 := "sitting"
+	dist1 := levenstheinDistance(s1, t1)
+	fmt.Printf("Levenshtein distance between %q and %q is %d.\n", s1, t1, dist1)
+	s2 := "book"
+	t2 := "back"
+	dist2 := levenstheinDistance(s2, t2)
+	fmt.Printf("Levenshtein distance between %q and %q is %d.\n", s2, t2, dist2)
+	s3 := "penrose"
+	t3 := "rose"
+	dist3 := levenstheinDistance(s3, t3)
+	fmt.Printf("Levenshtein distance between %q and %q is %d.\n", s3, t3, dist3)
+	s4 := "Something that matters"
+	t4 := "It doesn't matter"
+	dist4 := levenstheinDistance(s4, t4)
+	fmt.Printf("Levenshtein distance between %q and %q is %d.\n", s4, t4, dist4)
+	fmt.Println()
+
+	fmt.Println("Similarity-test:")
+	simil1 := calculateSimilarity(s1, t1)
+	fmt.Printf("Similarity between %q and %q is %f.\n", s1, t1, simil1)
+	simil2 := calculateSimilarity(s2, t2)
+	fmt.Printf("Similarity between %q and %q is %f.\n", s2, t2, simil2)
+	simil3 := calculateSimilarity(s3, t3)
+	fmt.Printf("Similarity between %q and %q is %f.\n", s3, t3, simil3)
+	simil4 := calculateSimilarity(s4, t4)
+	fmt.Printf("Similarity between %q and %q is %f.\n", s4, t4, simil4)
 	fmt.Println()
 }
